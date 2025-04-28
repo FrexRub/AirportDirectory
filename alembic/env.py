a8105ba90d8a9f187148a47bbf3510ef0bc41cae
Станислав Рубцов <frex@mail.ru>
2025-04-28 21:27:45 +0300

    create table users

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from src.core.config import setting
+from src.models import user
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.core.database import Base
+from src.models.base import Base
 
 target_metadata = Base.metadata
 
