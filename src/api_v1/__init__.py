499d003350fc5696ffb7e0c35afe50fe37427f0c
Станислав Рубцов <frexrub@gmail.com>
2025-07-11 10:47:43 +0300

    api: add cities_router

--- src/api_v1/__init__.py
+++ src/api_v1/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from .airports.views import router as airports_router
 from .users.views import router as users_router
+from .cities.views import router as cities_router
 
 router = APIRouter(prefix="/api")
 router.include_router(router=users_router)
 router.include_router(router=airports_router)
+router.include_router(router=cities_router)
