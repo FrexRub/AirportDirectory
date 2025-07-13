ba586cbe57109889dc913aab74fcfad583835f81
Станислав Рубцов <frex@mail.ru>
2025-07-13 23:05:16 +0300

    test: add test_city

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-TODO
-
-
-Накатить миграции
 ```
 docker compose exec app alembic upgrade head
 ```
 
-Добавить данные в базу данных
 ```
 docker compose exec app docker/add_data.sh
+```
+
+Сертификат от Let's Encrypt для домена airportcards.ru
+```
+docker run -it --rm --name certbot -v "/opt/webserver/nginx/ssl:/etc/letsencrypt" -v "/opt/webserver/nginx/well-known:/usr/share/nginx/html" certbot/certbot certonly --agree-tos --email frex@mail.ru -d airportcards.ru -d www.airportcards.ru
+```
+
+Для продления сертификата используем команду:
+```
+docker run -it --rm --name certbot -v "/opt/webserver/nginx/ssl:/etc/letsencrypt" -v "/opt/webserver/nginx/well-known:/usr/share/nginx/html" certbot/certbot renew
 ```
 
 Перечитать конфигурацию nginx для проверки на ошибки
--- src/utils/add_city_to_db.py
+++ src/utils/add_city_to_db.py
@@ -6,6 +6,7 @@ from typing import Any, Hashable
 import pandas as pd
 from sqlalchemy import exists, select
 from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import BASE_DIR, configure_logging
 from src.core.database import async_session_maker
@@ -53,6 +54,27 @@ async def city_from_files_to_db() -> None:
             else:
                 logger.info("The city %s is already in the database" % i_data["city"])
         await session.commit()
+
+
+async def city_from_files_to_test_db(session: AsyncSession) -> None:
+    """
+    Утилита добавления данных в тестовую бд
+    """
+    logger.info("Start add city to test db")
+    folder_path: Path = Path(BASE_DIR / "data")
+
+    df = pd.read_excel(folder_path / FILE_NAME)
+    df = df.dropna(how="any")
+
+    first_eight_records = df.head(8)
+
+    all_data: list[dict[Hashable, Any]] = first_eight_records.to_dict("records")
+
+    for i_data in all_data:
+        city: City = await data_to_model(i_data)
+        session.add(city)
+
+    await session.commit()
 
 
 if __name__ == "__main__":
