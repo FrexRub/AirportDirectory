97fffccb5e7fa2d367440b410485d8dc2a507d62
Станислав Рубцов <frexrub@gmail.com>
2025-07-29 14:43:57 +0300

    main: edit origin

--- src/main.py
+++ src/main.py
@@ -30,14 +30,16 @@ app = FastAPI(
     docs_url="/docs",
 )
 
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://airportcards.ru",
+    "https://www.airportcards.ru",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*",
-        # "http://localhost:8000",
-        # "https://airportcards.ru",
-        # "https://www.airportcards.ru",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
