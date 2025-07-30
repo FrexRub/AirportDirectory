a9eda1aaf26b6ddbe8dbc4ed3c0ee6cfe0175d5b
Станислав Рубцов <frex@mail.ru>
2025-07-30 21:57:08 +0300

    mail: change origins

--- src/main.py
+++ src/main.py
@@ -31,16 +31,18 @@ app = FastAPI(
 )
 
 origins = [
+    "http://localhost",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1",
     "https://airportcards.ru",
     "https://www.airportcards.ru",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    # allow_origins=origins,
+    # allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
