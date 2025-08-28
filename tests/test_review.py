3038579d78e0d46d4d68ea61cdc2060cfd31c3d2
Станислав Рубцов <frexrub@gmail.com>
2025-08-28 15:29:06 +0300

    tests: create test_reviews

---
+++ tests/test_review.py
@@ -0,0 +1,50 @@
+import asyncio
+
+from httpx import AsyncClient
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.comment import AirportComment
+from src.models.airport import Airport
+
+
+async def test_create_reviews_email_confirmed(
+    event_loop: asyncio.AbstractEventLoop,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    token_admin: str,
+):
+    stmt = select(Airport).filter(Airport.name == "Шереметьево")
+    result = await test_db.execute(stmt)
+    airport = result.scalars().one_or_none()
+
+    data = {"content": "Всё отлично", "rating": 5, "airport_id": str(airport.id)}
+    header = {"Authorization": f"Bearer {token_admin}"}
+    response = await client.post(
+        "api/reviews",
+        json=data,
+        headers=header,
+    )
+    assert response.status_code == 201
+
+
+async def test_create_reviews_email_unconfirmed(
+    event_loop: asyncio.AbstractEventLoop,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    token_user: str,
+):
+    stmt = select(Airport).filter(Airport.name == "Шереметьево")
+    result = await test_db.execute(stmt)
+    airport = result.scalars().one_or_none()
+
+    data = {"content": "Всё отлично", "rating": 5, "airport_id": str(airport.id)}
+    header = {"Authorization": f"Bearer {token_user}"}
+    response = await client.post(
+        "api/reviews",
+        json=data,
+        headers=header,
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Необходимо подтвердить почту"}
