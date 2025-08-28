862b0d5421476f874740cbe8aab0962398228103
Станислав Рубцов <frexrub@gmail.com>
2025-08-28 15:59:14 +0300

    tests: add test_create_reviews_bad_rating

--- tests/test_review.py
+++ tests/test_review.py
@@ -48,3 +48,26 @@ async def test_create_reviews_email_unconfirmed(
     )
     assert response.status_code == 400
     assert response.json() == {"detail": "Необходимо подтвердить почту"}
+
+
+async def test_create_reviews_bad_rating(
+    event_loop: asyncio.AbstractEventLoop,
+    client: AsyncClient,
+    test_db: AsyncSession,
+    token_admin: str,
+):
+    stmt = select(Airport).filter(Airport.name == "Шереметьево")
+    result = await test_db.execute(stmt)
+    airport = result.scalars().one_or_none()
+
+    data = {"content": "Всё отлично", "rating": 10, "airport_id": str(airport.id)}
+    header = {"Authorization": f"Bearer {token_admin}"}
+    response = await client.post(
+        "api/reviews",
+        json=data,
+        headers=header,
+    )
+    detail = response.json()["detail"]
+    print("------>", detail)
+    assert response.status_code == 422
+    assert detail[0]["msg"] == "Input should be less than or equal to 5"
