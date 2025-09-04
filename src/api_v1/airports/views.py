2c32a52571ff48727b2bde873260a2e8ede8bf21
Станислав Рубцов <frex@mail.ru>
2025-09-04 22:24:42 +0300

    airports: change query params to path

--- src/api_v1/airports/views.py
+++ src/api_v1/airports/views.py
@@ -61,20 +61,20 @@ async def get_airports_all(
     return paginate(airports_db)
 
 
-@router.get("/airport", response_model=AirPortOutAllSchemas)
+@router.get("/airport/{airport_id}", response_model=AirPortOutAllSchemas)
 async def get_airport_by_id(
-    id: UUID,
+    airport_id: UUID,
     session: AsyncSession = Depends(get_async_session),
     db_cache=Depends(get_cache_connection),
 ) -> AirPortOutAllSchemas:
     """
     Возвращает данные аэропорта по ID
     """
-    logger.info("Start load info about airport with id %s", str(id))
-    airport_json: str = await db_cache.get(str(id))
+    logger.info("Start load info about airport with id %s", str(airport_id))
+    airport_json: str = await db_cache.get(str(airport_id))
     if airport_json is None:
         try:
-            airport: Airport = await get_airport(session=session, id_airport=id)
+            airport: Airport = await get_airport(session=session, id_airport=airport_id)
         except ExceptDB as exp:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
@@ -87,10 +87,10 @@ async def get_airport_by_id(
             )
         else:
             airport_json_model: str = await model_to_json(pydantic_model=AirPortOutAllSchemas, object=airport)
-            await db_cache.set(str(id), airport_json_model, ex=CACHE_EXP)
-            logger.info("Write in cache info about airport with id %s", str(id))
+            await db_cache.set(str(airport_id), airport_json_model, ex=CACHE_EXP)
+            logger.info("Write in cache info about airport with id %s", str(airport_id))
     else:
-        logger.info("Read from cache info about airport with id %s", str(id))
+        logger.info("Read from cache info about airport with id %s", str(airport_id))
         airport: AirPortOutAllSchemas = await json_to_model(  # type: ignore
             pydantic_model=AirPortOutAllSchemas, json_object=airport_json
         )  # type: ignore
